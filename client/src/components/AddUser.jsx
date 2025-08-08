7f509ae9d7b94fcbaa920264db445d11e0d9bb56
Ana Beatriz <anabeatrizdscotrim@gmail.com>
2025-08-07 21:16:10 -0300

    Enviando arquivos

---
+++ client/src/components/AddUser.jsx
@@ -0,0 +1,135 @@
+import { Dialog } from "@headlessui/react";
+import React from "react";
+import { useForm } from "react-hook-form";
+import { useDispatch, useSelector } from "react-redux";
+import { toast } from "sonner";
+import { useRegisterMutation } from "../redux/slices/api/authApiSlice";
+import { useUpdateUserMutation } from "../redux/slices/api/userApiSlice";
+import { setCredentials } from "../redux/slices/authSlice";
+import { Button, Loading, ModalWrapper, Textbox } from "./";
+
+const AddUser = ({ open, setOpen, userData }) => {
+  let defaultValues = userData ?? {};
+  const { user } = useSelector((state) => state.auth);
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({ defaultValues });
+
+  const dispatch = useDispatch();
+
+  const [addNewUser, { isLoading }] = useRegisterMutation();
+  const [updateUser, { isLoading: isUpdating }] = useUpdateUserMutation();
+
+  const handleOnSubmit = async (data) => {
+    try {
+      if (userData) {
+        const res = await updateUser(data).unwrap();
+        toast.success(res?.message);
+        if (userData?._id === user?._id) {
+          dispatch(setCredentials({ ...res?.user }));
+        }
+      } else {
+        const res = await addNewUser({
+          ...data,
+          password: data?.email,
+        }).unwrap();
+        toast.success("Usuário adicionado com sucesso");
+      }
+
+      setTimeout(() => {
+        setOpen(false);
+      }, 1500);
+    } catch (err) {
+      console.log(err);
+      toast.error(err?.data?.message || err.error);
+    }
+  };
+
+  return (
+    <>
+      <ModalWrapper open={open} setOpen={setOpen}>
+        <form onSubmit={handleSubmit(handleOnSubmit)} className=''>
+          <Dialog.Title
+            as='h2'
+            className='text-base font-bold leading-6 text-gray-900 mb-4'
+          >
+            {userData ? "EDITAR PERFIL" : "ADICIONAR NOVO USUÁRIO"}
+          </Dialog.Title>
+          <div className='mt-2 flex flex-col gap-6'>
+            <Textbox
+              placeholder='Nome Completo'
+              type='text'
+              name='name'
+              label='Nome Completo'
+              className='w-full rounded'
+              register={register("name", {
+                required: "O nome é obrigatório!",
+              })}
+              error={errors.name ? errors.name.message : ""}
+            />
+            <Textbox
+              placeholder='Cargo'
+              type='text'
+              name='title'
+              label='Título'
+              className='w-full rounded'
+              register={register("title", {
+                required: "O cargo é obrigatório!",
+              })}
+              error={errors.title ? errors.title.message : ""}
+            />
+            <Textbox
+              placeholder='Email'
+              type='email'
+              name='email'
+              label='Email'
+              className='w-full rounded'
+              register={register("email", {
+                required: "O email é obrigatório!",
+              })}
+              error={errors.email ? errors.email.message : ""}
+            />
+
+            <Textbox
+              placeholder='Tipo de Usuário'
+              type='text'
+              name='role'
+              label='Tipo de Usuário'
+              className='w-full rounded'
+              register={register("role", {
+                required: "O tipo é obrigatório!",
+              })}
+              error={errors.role ? errors.role.message : ""}
+            />
+          </div>
+
+          {isLoading || isUpdating ? (
+            <div className='py-5'>
+              <Loading />
+            </div>
+          ) : (
+            <div className='py-3 mt-4 sm:flex sm:flex-row-reverse'>
+              <Button
+                type='submit'
+                className='bg-blue-400 px-4 text-sm font-semibold text-white hover:bg-blue-200  sm:w-auto'
+                label='Enviar'
+              />
+
+              <Button
+                type='button'
+                className= 'px-4 text-sm font-semibold text-gray-700 sm:w-auto ml'
+                onClick={() => setOpen(false)}
+                label='Cancelar'
+              />
+            </div>
+          )}
+        </form>
+      </ModalWrapper>
+    </>
+  );
+};
+
+export default AddUser;
---
+++ client/src/components/Sidebar.jsx
@@ -0,0 +1,116 @@
+import clsx from "clsx";
+import React from "react";
+import { FaUsers } from "react-icons/fa";
+import {
+  MdDashboard,
+  MdOutlinePendingActions,
+  MdSettings,
+  MdTaskAlt,
+} from "react-icons/md";
+import { useDispatch, useSelector } from "react-redux";
+import { Link, useLocation } from "react-router-dom";
+import { setOpenSidebar } from "../redux/slices/authSlice";
+import { IoCheckmarkDoneOutline } from "react-icons/io5";
+import { LuTrash2 } from "react-icons/lu";
+import { GiCheckMark } from "react-icons/gi";
+import { RiFileList2Line } from "react-icons/ri";
+
+const linkData = [
+  {
+    label: "Dashboard",
+    link: "dashboard",
+    icon: <MdDashboard />,
+  },
+  {
+    label: "Tarefas",
+    link: "tasks",
+    icon: <RiFileList2Line />,
+  },
+  {
+    label: "Tarefas Finalizadas",
+    link: "completed/completed",
+    icon: <MdTaskAlt />,
+  },
+  {
+    label: "Tarefas em Progresso",
+    link: "in-progress/in progress",
+    icon: <MdOutlinePendingActions />,
+  },
+  {
+    label: "Tarefas Para Fazer",
+    link: "todo/todo",
+    icon: <MdOutlinePendingActions />,
+  },
+  {
+    label: "Time",
+    link: "team",
+    icon: <FaUsers />,
+  },
+  {
+    label: "Status",
+    link: "status",
+    icon: <IoCheckmarkDoneOutline />,
+  },
+  {
+    label: "Lixo",
+    link: "trashed",
+    icon: <LuTrash2 />,
+  },
+];
+
+const Sidebar = () => {
+  const { user } = useSelector((state) => state.auth);
+
+  const dispatch = useDispatch();
+  const location = useLocation();
+  const path = location.pathname.split("/")[1];
+  const sidebarLinks = user?.isAdmin ? linkData : linkData.slice(0, 5);
+
+  const closeSidebar = () => {
+    dispatch(setOpenSidebar(false));
+  };
+
+  const NavLink = ({ el }) => {
+    return (
+      <Link
+        onClick={closeSidebar}
+        to={el.link}
+        className={clsx(
+          "w-fult lg:w-3/4 flex gap-2 px-3 py-2 rounded-full items-center text-gray-800 dark:text-gray-400 text-base hover:bg-[#e3e4e4]",
+          path === el.link.split("/")[0] ? "bg-blue-400 text-white" : ""
+        )}
+      >
+        {el.icon}
+        <span className='hover:text-[#2c3744]'>{el.label}</span>
+      </Link>
+    );
+  };
+
+  return (
+    <div className='w-full h-full flex flex-col gap-6 p-5'>
+      <h1 className='flex gap-1 items-center'>
+        <p className='bg-blue-400 p-2 rounded-full'>
+          <GiCheckMark className='text-white text-2xl font-black' />
+        </p>
+        <span className='text-2xl font-bold text-black dark:text-white'>
+          FlowFix
+        </span>
+      </h1>
+
+      <div className='flex-1 flex flex-col gap-y-5 py-8'>
+        {sidebarLinks.map((link) => (
+          <NavLink el={link} key={link.label} />
+        ))}
+      </div>
+
+      <div className=''>
+        <button className='w-full flex gap-2 p-2 items-center text-lg text-gray-800 dark:text-white'>
+          <MdSettings />
+          <span>Configurações</span>
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Sidebar;
